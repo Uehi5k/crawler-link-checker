6be5c4e5c516339fd14490d63a1ff8c192783314
Chau Hoang Hieu Lam <lamchauhoanghieu@gmail.com>
2025-02-19 16:08:31 +1100

    feat: Add file check if exist before returning file

--- src/elysia-routes/crawl.ts
+++ src/elysia-routes/crawl.ts
@@ -1,17 +1,22 @@
 import { Elysia, error, file, t } from "elysia";
 import { crawlUrl, crawler, getCrawlInfo } from "../services/crawl.service.js";
 import { getResults } from "../services/results.service.js";
-
+import { existsSync } from "fs";
 class Crawler {
   // TODO - to improve this and have another route to request for progress
   crawl(url: string) {
     const { domain, datasetStorage } = getCrawlInfo(url);
     if (domain === null) {
-      return error(400, { error: "URL is not correct, please try a different domain!" });
+      return error(400, {
+        error: "URL is not correct, please try a different domain!",
+      });
     }
 
     if (crawler !== undefined) {
-      return error(503, { error: "Crawler is currently performing for a user, please try again later!" });
+      return error(503, {
+        error:
+          "Crawler is currently performing for a user, please try again later!",
+      });
     }
 
     crawlUrl(url, domain, datasetStorage, false);
@@ -38,5 +43,16 @@ export const crawl = new Elysia({ prefix: "/crawl" })
   )
   .get("results/:id", ({ params: { id }, crawler }) => crawler.getResults(id))
   .get("results/download/:id", ({ params: { id } }) => {
-    return file(`./storage/key_value_stores/${id}/${id}.csv`);
+    try {
+      const path = `./storage/key_value_stores/${id}/${id}.csv`;
+      const fileExist = existsSync(path);
+      if (!fileExist) {
+        return error(404, { error: "File not found" });
+      }
+
+      const resultFile = file(path);
+      return resultFile;
+    } catch {
+      return error(404, { error: "File not found" });
+    }
   });
